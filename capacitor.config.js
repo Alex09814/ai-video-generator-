6779045ecd4bf50784c6a87e2d2d17278c40da22
Alex09814 <mdd132339@gmail.com>
2026-09-21 19:16:10 +0530

    Add initial capacitor configuration file

---
+++ capacitor.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  appId: 'com.videoforge.ai',
+  appName: 'VideoForge AI',
+  webDir: 'out',
+
+  server: {
+    url: 'https://ai-video-generator-g3j5hja9c-alex09814.vercel.app/game',
+    cleartext: false
+  }
+};
