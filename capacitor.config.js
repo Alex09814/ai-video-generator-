a55b9ce08b7eea5a5d428f57fe281e317ea8db42
Alex09814 <mdd132339@gmail.com>
2026-09-21 19:20:57 +0530

    Change webDir from 'out' to 'public'

--- capacitor.config.js
+++ capacitor.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   appId: 'com.videoforge.ai',
   appName: 'VideoForge AI',
-  webDir: 'out',
+  webDir: 'public',
 
   server: {
     url: 'https://ai-video-generator-g3j5hja9c-alex09814.vercel.app/game',
